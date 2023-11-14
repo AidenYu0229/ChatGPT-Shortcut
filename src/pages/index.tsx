a6f899ea6ffae8d8425dcd82b0e37094f08d8542
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-11-14 14:48:48 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -153,7 +153,7 @@ function ShowcaseHeader() {
   return (
     <section className={styles.mobileMarginAdjust + " text--center"}>
       <Heading as='h1' className={styles.hideOnMobile}>
-        AI Short
+        AI 提示词
       </Heading>
       <p>{SLOGAN}</p>
       <UserStatus hideLinks={{ userCenter: true, myFavorite: false }} />
